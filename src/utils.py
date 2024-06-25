b926d48d00b03498207d47f7dcc926f9b101c853
Kaustubh <22222@iiitu.ac.in>
2024-06-25 21:21:25 +0530

    Hypertuning

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,11 @@
 import os
 import sys
 import dill
+import pickle
 
 from src.exception import CustomException
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
@@ -13,15 +15,33 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_models(x_train, y_train,x_test,y_test,models):
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def evaluate_models(x_train, y_train,x_test,y_test,models,param):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            
+            para=param[list(models.keys())[i]]
 
-            model.fit(x_train, y_train)  # Train model
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(x_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(x_train,y_train)
+
+            #model.fit(X_train, y_train)  # Train model
 
             y_train_pred = model.predict(x_train)
 
@@ -37,7 +57,4 @@ def evaluate_models(x_train, y_train,x_test,y_test,models):
 
     except Exception as e:
         raise CustomException(e, sys)
-
-
-
-
+    
